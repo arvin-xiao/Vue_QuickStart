7f1c73edaaeead3e086fe55b4981d8768e65328c
Arvin Xiao <1243276550@qq.com>
2018-07-18 20:31:36 +0800

    Vue.QuickStart

---
+++ Vue_Test/js/login.js
@@ -0,0 +1,7 @@
+const login ={
+    template:'<div>\n' +
+    '        <h3>登录页</h3>\n' +
+    '        用户：<input type="text" name="user"><br>\n' +
+    '        密码：<input type="text" name="password">\n' +
+    '    </div>'
+};
---
+++ Vue_Test/js/register.js
@@ -0,0 +1,8 @@
+const register = {
+    template:'<div>\n' +
+    '        <h3>注册页</h3>\n' +
+    '        用户：<input type="text" name="user"><br>\n' +
+    '        密码：<input type="text" name="password"><br>\n' +
+    '        确认密码：<input type="text" name="password">\n' +
+    '    </div>'
+};
