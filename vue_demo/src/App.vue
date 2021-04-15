d866814b1c4b53276c4d2405625693a10e67af8f
arvin.xiao <arvin.xiao@huimeihealth.com.cn>
2021-04-15 18:55:24 +0800

    vue ui

---
+++ vue_demo/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-row>
+        <el-button>默认按钮</el-button>
+        <el-button type="primary">主要按钮</el-button>
+        <el-button type="success">成功按钮</el-button>
+        <el-button type="info">信息按钮</el-button>
+        <el-button type="warning">警告按钮</el-button>
+        <el-button type="danger">危险按钮</el-button>
+      </el-row>
+
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  created(){
+    console.log('初始化');
+    axios.get('user')
+    .then(
+      function(response){
+        console.log(response);
+      }
+    ).catch(
+      function(error){
+        console.log(error);
+      }
+    )
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
